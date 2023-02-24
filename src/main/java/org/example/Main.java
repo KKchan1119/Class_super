57194b7e61778a439b583d5cb62d3e0c248dccb7
KKchan1119 <kkchan1119@gmail.com>
2023-02-24 13:34:06 +0900

    전사 두명 무기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,63 @@
 package org.example;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        전사 a전사 = new 홍길동();
+        a전사.a무기 = new 활();
+
+        a전사.공격();
+        // 출력 => 홍길동이(가) 활(으)로 공격합니다.
+
+        a전사 = new 홍길순();
+
+        a전사.공격();
+        // 출력 => 홍길순이(가) 칼(으)로 공격합니다.
+
+        a전사.a무기 = new 창();
+        a전사.공격();
+        // 출력 => 홍길순이(가) 창(으)로 공격합니다.
+    }
+}
+
+class 전사{
+    String 이름;
+    무기 a무기;
+    public void 공격() {
+        a무기.작동(이름);
+    }
+}
+class 홍길동 extends 전사 {
+    홍길동(){
+        이름 = "홍길동";
+        this.a무기 = new 활();
+    }
+}
+class 홍길순 extends 전사{
+
+    홍길순(){
+        이름 = "홍길순";
+        this.a무기 = new 칼();
+    }
+}
+class 무기{
+    String 무기명;
+    void 작동(String 사용자명){
+        System.out.println(사용자명 + "(이)가" + 무기명 + "으로 공격합니다");
+
+    }
+}
+class 칼 extends 무기{
+    칼(){
+        무기명 = "칼";
+    }
+}
+class 활 extends 무기{
+    활(){
+        무기명 = "활";
+    }
+}
+class 창 extends 무기{
+    창(){
+        무기명 = "창";
     }
 }
